2319fcca314555739619495a8366d1854f93ac8a
Rafal Dunaj <Rafal_Dunaj@epam.com>
2019-12-16 16:01:09 +0100

    changed api router to tasks

--- src/Server/app.js
+++ src/Server/app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const tasksRouter = require('./routes/tasks');
 
 const app = express();
 
@@ -18,10 +18,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/tasks', tasksRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- src/Server/routes/tasks.js
+++ src/Server/routes/tasks.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET users listing. */
+/* GET tasks listing. */
 router.get('/', (req, res) => {
   res.send('respond with a resource');
+});
+
+router.get('/:id', (req, res) => {
+  res.send('respond with a resource');
+});
+
+router.post('/', (req, res) => {
+  res.status(200);
 });
 
 module.exports = router;
